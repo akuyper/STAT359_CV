56d0f991c4481a6c38b813984162ea41064b9238
Austin Kim <57238316+austinkim118@users.noreply.github.com>
2022-03-06 17:25:12 -0600

    trips

--- trips.R
+++ trips.R
@@ -1,144 +1,173 @@
-library(tidyverse)
-library(dplyr)
 
-## read in rides by communities data
-trips <- read.csv("data/processed/rides_community.csv")
+# Set Up Packages and Data ------------------------------------------------
 
-# sample data because lag ----
-trips_sample <- trips %>% 
-  head(1000)
+# load packages
+library(tidyverse, lib = "/projects/e30686/R/4.1")
+library(arrow, lib = "/projects/e30686/R/4.1")
+library(dplyr, lib = "/projects/e30686/R/4.1")
+library(lubridate, lib = "/projects/e30686/R/4.1")
+library(janitor)
+## if janitor package is saying it is not found in shared package library, install it on your Quest and use the code below instead
+# library(janitor)
 
-# select only the columns we're interested in
-where <- trips_sample %>% 
-  select(to_station_name, from_station_name, to_community, from_community)
+library(sf) 
+## sf package is not in shared package library on Quest
+## users need to paste code below into their console to install sf ----
+# Sys.setenv(INCLUDE = "/software/geos/3.8.1/include:/software/gcc/8.4.0/include:/software/proj/7.1.1/include")
+# Sys.setenv(PKG_CONFIG_PATH = "/software/sqlite/3.27.2/lib/pkgconfig:/software/proj/7.1.1/lib/pkgconfig:/software/gdal/3.1.3-R-4.1.1/lib/pkgconfig")
+# old_path <- Sys.getenv("PATH")
+# Sys.setenv(PATH = paste("/software/geos/3.8.1/bin", old_path, sep = ":"))
+# old_path <- Sys.getenv("PATH")
+# Sys.setenv(PATH = paste("/software/proj/7.1.1/bin", old_path, sep = ":"))
+# old_path <- Sys.getenv("PATH")
+# Sys.setenv(PATH = paste("/software/gdal/3.1.3-R-4.1.1/bin", old_path, sep = ":"))
+# install.packages("sf", repos="https://cloud.r-project.org/")
 
+# if there are further issues with sf package, refer to https://kb.northwestern.edu/troubleshooting-installing-r-packages#SF
 
-### STATIONS ----
-# To and From stations/communities and # of rides from stations
-where %>% 
-  group_by(to_station_name) %>% 
-  mutate(end = n()) %>% 
-  group_by(from_station_name) %>% 
-  mutate(start = n()) %>% 
-  select(to_station_name, from_station_name, end, start) 
+## Coordinate points into stations/
+# read in data with lat-long coordinates you want matched to community area (station stats)
+trip_data <- read_csv_arrow("/projects/e30686/data/raw/trip_data.csv")
+stations_communites <- read_csv_arrow("/projects/e30686/data/Full Network Station Install Dates.csv")
 
-# station name and # of rides ended at that station
-to_station <- where %>% 
-  group_by(to_station_name) %>% 
-  summarise(end = n()) %>% 
-  select(to_station_name, end) %>% 
-  group_by(to_station_name) %>% 
-  rename(station = to_station_name)
+# yearly data 
+from_year <- trip_data %>% 
+  mutate(
+    year = year(start_time)
+  ) %>% 
+  count(year, from_station_name)
 
-# station name and # of rides started at that station
-from_station <- where %>% 
-  group_by(from_station_name) %>% 
-  summarise(start = n()) %>% 
-  select(from_station_name, start) %>% 
-  group_by(from_station_name) %>% 
-  rename(station = from_station_name)
+stations_communites <- stations_communites %>% 
+  select(name, community_area, `install date`)
 
-# combined above two tables
-stations <- merge(from_station, to_station, by = "station")
+from_community <- merge(from_year, stations_communites, by.x = "from_station_name", by.y = "name")
 
-### COMMUNITIES ----
+to_year <- trip_data %>% 
+  mutate(
+    year = year(stop_time)
+  ) %>% 
+  count(year, to_station_name)
 
-# station name and # of rides ended at that station
-to_community_name <- where %>% 
-  group_by(to_community) %>% 
-  summarise(end = n()) %>% 
-  select(to_community, end) %>% 
-  group_by(to_community) %>% 
-  rename(community = to_community)
+to_community <- merge(to_year, stations_communites, by.x = "to_station_name", by.y = "name")
 
-# station name and # of rides started at that station
-from_community_name <- where %>% 
-  group_by(from_community) %>% 
-  summarise(start = n()) %>% 
-  select(from_community, start) %>% 
-  group_by(from_community) %>% 
-  rename(community = from_community)
+pct_from_year <- from_year %>% 
+  group_by(year) %>% 
+  mutate(
+    pct_rides = 100 * n / sum(n)
+  ) 
 
-community <- merge(from_community_name, to_community_name, by = "community")
+pct_to_year <- to_year %>% 
+  group_by(year) %>% 
+  mutate(
+    pct_rides = 100 * n / sum(n)
+  )
 
+pct_year <- left_join(
+  pct_from_year, 
+  pct_to_year, 
+  by = c("from_station_name" = "to_station_name", "year" = "year")
+) %>% 
+  rename(
+    station_name = from_station_name,
+    from = n.x,
+    pct_from = pct_rides.x,
+    to = n.y,
+    pct_to = pct_rides.y
+  )
 
-##### community names and number of stations in each community ----
+## final dataset
+from_to_year <- left_join(
+  from_year, 
+  to_year, 
+  by = c("from_station_name" = "to_station_name", "year" = "year")
+) %>% 
+  rename(
+    station_name = from_station_name,
+    from = n.x,
+    to = n.y
+  ) %>% 
+  mutate(
+    pct_return = 100 * to / from
+  ) 
 
-number_of_stations <- where %>% 
-  distinct(to_station_name, to_community) %>% 
-  group_by(to_community) %>% 
-  summarise(stations = n()) %>% 
-  rename(community = to_community)
+final_year <- left_join(
+  from_to_year,
+  pct_year,
+  by = c("station_name", "year", "from", "to")
+)
 
-communities <- merge(community, number_of_stations, by = "community")
+## use ----
 
+data_2019 <- from_to_year %>% 
+  filter(year == 2019)
 
-#### TRIP DATA----
+data_2019_accesibility <- data_2019 %>% 
+  filter(pct_return < 90) %>% 
+  arrange(desc(pct_return))
 
-Where <- trips %>% 
-  select(to_station_name, from_station_name, to_community, from_community)
+# monthly data ----
+from_month <- trip_data %>% 
+  mutate(
+    year = year(start_time),
+    month = month(start_time)
+  ) %>% 
+  count(year, month, from_station_name)
 
-### STATIONS ----
-# To and From stations/communities and # of rides from stations
-Where %>% 
-  group_by(to_station_name) %>% 
-  mutate(end = n()) %>% 
-  group_by(from_station_name) %>% 
-  mutate(start = n()) %>% 
-  select(to_station_name, from_station_name, end, start) 
+from_community <- merge(from_month, stations_communites, by.x = "from_station_name", by.y = "name")
 
-# station name and # of rides ended at that station
-To_Station <- Where %>% 
-  group_by(to_station_name) %>% 
-  summarise(end = n()) %>% 
-  select(to_station_name, end) %>% 
-  group_by(to_station_name) %>% 
-  rename(station = to_station_name)
+to_month <- trip_data %>% 
+  mutate(
+    year = year(stop_time),
+    month = month(stop_time)
+  ) %>% 
+  count(year, month, to_station_name)
 
-# station name and # of rides started at that station
-From_Station <- Where %>% 
-  group_by(from_station_name) %>% 
-  summarise(start = n()) %>% 
-  select(from_station_name, start) %>% 
-  group_by(from_station_name) %>% 
-  rename(station = from_station_name)
+to_community <- merge(to_month, stations_communites, by.x = "to_station_name", by.y = "name")
 
-# combined above two tables
-Stations <- merge(From_Station, To_Station, by = "station")
+## percentages 
+pct_from_month <- from_month %>% 
+  group_by(year, month) %>% 
+  mutate(
+    pct_rides = 100 * n / sum(n)
+  ) 
 
-write_csv(Stations, "data/processed/trips_stations.csv")
+pct_to_month <- to_month %>% 
+  group_by(year, month) %>% 
+  mutate(
+    pct_rides = 100 * n / sum(n)
+  )
 
-### COMMUNITIES ----
-# select only the columns we're interested in
+pct_month <- left_join(
+  pct_from_month, 
+  pct_to_month, 
+  by = c("from_station_name" = "to_station_name", "month" = "month", "year" = "year")
+) %>% 
+  rename(
+    station_name = from_station_name,
+    from = n.x,
+    pct_from = pct_rides.x,
+    to = n.y,
+    pct_to = pct_rides.y
+  )
 
-To_Community_Name <- Where %>% 
-  group_by(to_community) %>% 
-  summarise(end = n()) %>% 
-  select(to_community, end) %>% 
-  group_by(to_community) %>% 
-  rename(community = to_community)
+from_to_month <- left_join(
+  from_month, 
+  to_month, 
+  by = c("from_station_name" = "to_station_name", "month" = "month", "year" = "year")
+) %>% 
+  rename(
+    station_name = from_station_name,
+    from = n.x,
+    to = n.y
+  ) %>% 
+  mutate(
+    pct_return = 100 * to / from
+  ) 
 
-# station name and # of rides started at that station
-From_Community_Name <- Where %>% 
-  group_by(from_community) %>% 
-  summarise(start = n()) %>% 
-  select(from_community, start) %>% 
-  group_by(from_community) %>% 
-  rename(community = from_community)
-
-Community <- merge(From_Community_Name, To_Community_Name, by = "community")
-
-##### community names and number of stations in each community
-
-Number_Of_Stations <- Where %>% 
-  distinct(to_station_name, to_community) %>% 
-  group_by(to_community) %>% 
-  summarise(stations = n()) %>% 
-  rename(community = to_community)
-
-Communities <- merge(Community, Number_Of_Stations, by = "community")
-
-write_csv(Communities, "data/processed/trips_community.csv")
+final_month <- left_join(
+  from_to_month,
+  pct_month,
+  by = c("station_name", "month", "from", "to", "year")
 
 
 
